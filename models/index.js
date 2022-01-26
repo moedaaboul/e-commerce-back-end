3b41f8f322bc4ff5ab38dea716d145b95a6bc911
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-01-26 23:44:28 +0000

    adds association methods on Sequelize models to create relationships

--- models/index.js
+++ models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'categoryId',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'categoryId',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+ProductTag.belongsTo(Product, {
+  foreignKey: 'productId',
+});
 
 // Tags belongToMany Products (through ProductTag)
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tagId',
+});
 
 module.exports = {
   Product,
